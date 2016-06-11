b98cccbb2bcb50c480a6e9e9e1a2e30fb86b2294
Caspar Treijtel <c.treijtel@xs4all.nl>
2016-06-11 21:25:54 +0200

    Hooray! ediff window in same frame just as I want
    - ediff window now shows up at the bottom in a new window, that is
      automatically enlarged for the help (?)

--- init-package/init-magit.el
+++ init-package/init-magit.el
@@ -1,4 +1,6 @@
 ;;; Custom key bind for much used magit-status
 (global-set-key (kbd "C-c g s") 'magit-status)
 
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+
 (provide 'init-magit)
