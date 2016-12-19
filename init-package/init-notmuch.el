a4957c784ac626bd7711353d27fc613015bae8f1
Caspar Treijtel <c.treijtel@xs4all.nl>
2016-12-19 21:17:14 +0100

    Config for notmuch
    - Added to the repository, although I'm still on mu4e, maybe for future
      reference.

---
+++ init-package/init-notmuch.el
@@ -0,0 +1,18 @@
+;; configuration for notmuch
+
+(autoload 'notmuch "notmuch" "notmuch mail" t)
+
+;; Notmuch ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(evil-define-key 'normal notmuch-search-mode-map
+  (kbd "q") 'notmuch-search-quit
+  (kbd "n") 'notmuch-search-next-thread
+  (kbd "p") 'notmuch-search-previous-thread
+  (kbd "RET") 'notmuch-search-show-thread)
+
+;; Mail ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(evil-add-hjkl-bindings notmuch-search-mode-map 'emacs)
+(evil-add-hjkl-bindings notmuch-show-mode-map 'emacs)
+(evil-set-initial-state 'notmuch-search-mode 'emacs)
+(evil-set-initial-state 'notmuch-show-mode 'emacs)
+
+(provide 'init-mail)
