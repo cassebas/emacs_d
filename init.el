6df2fe7d81bc102b2bcac2b0bf86b3414f46f4d8
Caspar Treijtel <c.treijtel@uva.nl>
2017-02-13 09:20:17 +0000

    Added GNU repository to list of pkg repositories

--- init.el
+++ init.el
@@ -1,6 +1,15 @@
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(add-to-list
+ 'package-archives
+ '("gnu" . "http://elpa.gnu.org/packages/")
+ t)
+
+(add-to-list
+ 'package-archives
+ '("melpa" . "https://melpa.org/packages/")
+ t)
+
 (package-initialize)
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
