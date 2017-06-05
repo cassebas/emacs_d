62330a6d633308e9ef712e33b4ab3c664b509023
Caspar Treijtel <c.treijtel@xs4all.nl>
2017-06-05 13:32:04 +0200

    Added pyenv-mode
    - Added pyenv-mode (be sure to install pyenv also).
    - Added a projectile hook that calls pyenv-mode-set for setting the
      pyenv for the project (assumption is that the pyenv's name equals
      the project name).

--- init-package/init-python.el
+++ init-package/init-python.el
@@ -1,5 +1,18 @@
 (install-package 'anaconda-mode)
+(install-package 'pyenv-mode)
 (add-hook 'python-mode-hook 'anaconda-mode)
+(add-hook 'python-mode-hook 'pyenv-mode)
+
+;; The following function will be called upon a projectile
+;; hook that will run when C-c p p is issued (projectile-switch-project)
+(defun projectile-pyenv-mode-set ()
+  "Set pyenv version matching project name."
+  (let ((project (projectile-project-name)))
+    (if (member project (pyenv-mode-versions))
+        (pyenv-mode-set project)
+      (pyenv-mode-unset))))
+
+(add-hook 'projectile-after-switch-project-hook 'projectile-pyenv-mode-set)
 
 (install-package 'company-anaconda)
 (eval-after-load "company"
