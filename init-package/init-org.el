6564a886969f6c57228f4cf3b7f817497ac38ab1
Caspar Treijtel <c.treijtel@xs4all.nl>
2017-06-08 21:02:26 +0200

    Special evil keybindings for org-mode

--- init-package/init-org.el
+++ init-package/init-org.el
@@ -6,4 +6,10 @@
 ;;;;; Turn on line numbers in org-mode
 (add-hook 'org-mode-hook (lambda () (linum-mode 1)))
 
+;;; Special org-mode keybindings for when using evil (like me:)
+(install-package 'evil-org)
+(require 'evil-org)
+(add-hook 'org-mode-hook 'evil-org-mode)
+(evil-org-set-key-theme '(navigation insert textobjects additional))
+
 (provide 'init-org)
