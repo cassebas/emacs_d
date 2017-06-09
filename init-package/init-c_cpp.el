47702e3a3b077dff7259967f534240afce36bd32
Caspar Treijtel <c.treijtel@xs4all.nl>
2017-06-09 20:43:00 +0200

    Change keybinding for company completion

--- init-package/init-c_cpp.el
+++ init-package/init-c_cpp.el
@@ -108,4 +108,10 @@
 (eval-after-load 'flycheck
   '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
 
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
 (provide 'init-c_cpp)
