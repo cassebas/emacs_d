ccc1b539b86fd911f54e6d323ffdc993d066a5d2
Caspar Treijtel <c.treijtel@xs4all.nl>
2017-11-18 18:06:23 +0100

    Java: no highlight current line, linum mode

--- init-package/init-java.el
+++ init-package/init-java.el
@@ -6,4 +6,9 @@
                                   tab-width 4
                                   indent-tabs-mode t)))
 
+(add-hook 'java-mode-hook (lambda ()
+                            (setq global-hl-line-mode nil)))
+
+(add-hook 'java-mode-hook 'linum-mode)
+
 (provide 'init-java)
