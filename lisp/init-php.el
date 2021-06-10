e5d0cbb0041ec114f0f5af8156a6ae97006e6693
Caspar Treijtel <c.treijtel@xs4all.nl>
2021-06-10 20:23:21 +0200

    Improve php config
    - in html mode turn on linum mode
    - comment fix

--- lisp/init-php.el
+++ lisp/init-php.el
@@ -6,12 +6,15 @@
 (use-package php-mode
   :ensure t)
 
-;; Show line numbers when editing python code.
+;; Show line numbers when editing php code.
 (add-hook 'php-mode-hook (lambda () (linum-mode 1)))
 
 ;; Also use web-mode
 (use-package web-mode
   :ensure t)
+
+;; Show line numbers when editing html code.
+(add-hook 'web-mode-hook (lambda () (linum-mode 1)))
 
 (provide 'init-php)
 ;;; init-php.el ends here
