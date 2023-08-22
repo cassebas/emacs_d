ee7822b4956479e7d6c676c222016823bec35f64
Caspar Treijtel <c.treijtel@hva.nl>
2023-08-22 12:12:18 +0200

    Updated straight.el configuration

--- init.el
+++ init.el
@@ -2,24 +2,26 @@
 
 ;;; Commentary:
 
+;; If using use-package, then this makes each use-package form also invoke straight.el
+;; to install the package, unless otherwise specified.
+(setq straight-use-package-by-default t)
+
 ;;; Code:
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+      (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
 ;;;;  Effectively replace use-package with straight-use-package
-;;; https://github.com/raxod502/straight.el/blob/develop/README.md#integration-with-use-package
 (straight-use-package 'use-package)
-(setq straight-use-package-by-default t)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
